dd552e37afe18586f1c5e2fee4eac405bc10ac05
Natalie Frydryck <n.frydryck@setonhill.edu>
2020-02-03 10:10:31 -0500

    Initial Commit

---
+++ Frydryck Homework-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Frydryck Homework-1
+//
+//  Created by Natalie Frydryck on 2/3/20.
+//  Copyright © 2020 Natalie Frydryck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Frydryck Homework-1Tests/Frydryck_Homework_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Frydryck_Homework_1Tests.swift
+//  Frydryck Homework-1Tests
+//
+//  Created by Natalie Frydryck on 2/3/20.
+//  Copyright © 2020 Natalie Frydryck. All rights reserved.
+//
+
+import XCTest
+@testable import Frydryck_Homework_1
+
+class Frydryck_Homework_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
